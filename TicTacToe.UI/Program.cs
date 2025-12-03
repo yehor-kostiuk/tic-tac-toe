e80f88d53e593fba1a1e02fe29c8c6332f1e1a14
Egor Kostiuk <kostukegor729@gmail.com>
2025-12-03 23:22:38 +0200

    feat: add ReactiveUI

--- TicTacToe.UI/Program.cs
+++ TicTacToe.UI/Program.cs
@@ -1,21 +1,16 @@
-﻿using Avalonia;
-using System;
+﻿using System;
+using Avalonia;
+using Avalonia.ReactiveUI;
 
-namespace TicTacToe.UI;
-
-class Program
+namespace TicTacToe.UI
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
+  class Program
+  {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+      BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+    public static AppBuilder BuildAvaloniaApp() =>
+      AppBuilder.Configure<App>().UsePlatformDetect().LogToTrace().UseReactiveUI();
+  }
 }
