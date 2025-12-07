7ef5eb0ebb31349f3ce0d088cc6a6b27f9f5e030
Egor Kostiuk <kostukegor729@gmail.com>
2025-12-07 20:05:47 +0200

    feat: add auth frontend features

--- TicTacToe.UI/App.axaml.cs
+++ TicTacToe.UI/App.axaml.cs
@@ -1,6 +1,9 @@
+using System; // TODO: delete
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using TicTacToe.BLL.Services;
+using TicTacToe.DAL.Repositories;
 using TicTacToe.UI.ViewModels;
 using TicTacToe.UI.Views;
 
@@ -17,10 +20,22 @@ public partial class App : Application
   {
     if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
     {
-      var authViewModel = new TicTacToe.UI.ViewModels.AuthViewModel();
-      var mainViewModel = new TicTacToe.UI.ViewModels.MainWindowViewModel(authViewModel);
+      var userRepo = new UserRepository();
+      var authService = new AuthService(userRepo);
 
-      desktop.MainWindow = new TicTacToe.UI.Views.MainWindow { DataContext = mainViewModel };
+      var authViewModel = new AuthViewModel(authService);
+      var mainViewModel = new MainWindowViewModel(authViewModel);
+
+      authViewModel.OnLoginSuccess = (user) =>
+      {
+        Console.WriteLine($"user {user.Username}");
+        Console.WriteLine($"rate {user.Rating}");
+        Console.WriteLine($"id {user.Id}");
+
+        authViewModel.Message = $"Вхід виконано {user.Username}";
+      };
+
+      desktop.MainWindow = new MainWindow { DataContext = mainViewModel };
     }
 
     base.OnFrameworkInitializationCompleted();
--- TicTacToe.UI/ViewModels/AuthViewModel.cs
+++ TicTacToe.UI/ViewModels/AuthViewModel.cs
@@ -1,33 +1,119 @@
+using System;
 using System.Reactive;
 using ReactiveUI;
+using TicTacToe.BLL.Services;
+using TicTacToe.Core.Entities;
 
 namespace TicTacToe.UI.ViewModels
 {
   public class AuthViewModel : ViewModelBase
   {
-    private string _pageTitle = "Сторінка Входу";
+    private readonly AuthService _authService;
 
-    public string PageTitle
+    public Action<User>? OnLoginSuccess { get; set; }
+
+    private string _username = string.Empty;
+    private string _password = string.Empty;
+    private string _email = string.Empty;
+    private string _message = string.Empty;
+    private bool _isRegisterMode;
+
+    public AuthViewModel(AuthService authService)
     {
-      get => _pageTitle;
-      set => this.RaiseAndSetIfChanged(ref _pageTitle, value);
+      _authService = authService;
+
+      SubmitCommand = ReactiveCommand.Create(ExecuteSubmit);
+
+      ToggleModeCommand = ReactiveCommand.Create(() =>
+      {
+        IsRegisterMode = !IsRegisterMode;
+        Message = string.Empty;
+      });
     }
 
-    public ReactiveCommand<Unit, Unit> SwitchPageCommand { get; }
-
-    public AuthViewModel()
+    public string Username
     {
-      SwitchPageCommand = ReactiveCommand.Create(() =>
+      get => _username;
+      set => this.RaiseAndSetIfChanged(ref _username, value);
+    }
+
+    public string Password
+    {
+      get => _password;
+      set => this.RaiseAndSetIfChanged(ref _password, value);
+    }
+
+    public string Email
+    {
+      get => _email;
+      set => this.RaiseAndSetIfChanged(ref _email, value);
+    }
+
+    public string Message
+    {
+      get => _message;
+      set => this.RaiseAndSetIfChanged(ref _message, value);
+    }
+
+    public bool IsRegisterMode
+    {
+      get => _isRegisterMode;
+      set
       {
-        if (PageTitle == "Сторінка Входу")
+        this.RaiseAndSetIfChanged(ref _isRegisterMode, value);
+        this.RaisePropertyChanged(nameof(MainButtonText));
+        this.RaisePropertyChanged(nameof(ToggleModeText));
+      }
+    }
+
+    public string MainButtonText => IsRegisterMode ? "Зареєструватися" : "Увійти";
+    public string ToggleModeText =>
+      IsRegisterMode ? "Вже є акаунт? Увійти" : "Немає акаунту? Реєстрація";
+
+    public ReactiveCommand<Unit, Unit> SubmitCommand { get; }
+    public ReactiveCommand<Unit, Unit> ToggleModeCommand { get; }
+
+    private void ExecuteSubmit()
+    {
+      if (IsRegisterMode)
+        Register();
+      else
+        Login();
+    }
+
+    private void Login()
+    {
+      try
+      {
+        var user = _authService.Login(Username, Password);
+        OnLoginSuccess?.Invoke(user);
+      }
+      catch (Exception ex)
+      {
+        Message = $"Помилка входу: {ex.Message}";
+      }
+    }
+
+    private void Register()
+    {
+      try
+      {
+        if (string.IsNullOrWhiteSpace(Username) || string.IsNullOrWhiteSpace(Password))
         {
-          PageTitle = "Сторінка Реєстрації";
+          Message = "Введіть логін та пароль";
+          return;
         }
-        else
-        {
-          PageTitle = "Сторінка Входу";
-        }
-      });
+
+        _authService.Register(Username, Password, Email);
+
+        Message = "Успішно! Тепер увійдіть";
+        IsRegisterMode = false;
+        Password = "";
+      }
+      catch (Exception ex)
+      {
+        Message = $"Помилка реєстрації: {ex.Message}";
+      }
     }
   }
 }
